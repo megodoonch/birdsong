b616a2bce26b46bd78a3a6d27732714d353b96c2
Meaghan Fowlie <Mego@Mego.local>
2016-10-12 01:01:42 -0400

    parser and probabilities working. Includes ignoring pointless buffer-clearing

--- markhov/markhov.py
+++ markhov/markhov.py
@@ -192,3 +192,169 @@ def generate(bigrams,operations,start=START_STATE):
     
     print ("sentence: %s\n"%(' '.join(state[0][0])))
     return state
+
+
+
+######### PARSE ###########
+
+def parse(s,bigrams):
+    """
+    Parses a string
+    Uses an agenda and a list of complete parses.
+    We initialise the agenda with a start state,   [['['],[],['mg'],False]
+    An agenda item is a list of 4 elements:
+    the string so far, the buffer, the operations, and whether the last "special" operation (copy or clear) is clear.
+    The last element is a little hack to reduce the amount of pointless buffer-clearing.
+    We add onto an agenda item until it is a complete parse, in which case we move it to the output, parses
+    Whenever there is a choice of two valid moves, we copy the agenda item and try both
+        
+    Arguments:
+    s       : the string to be parsed -- string
+    bigrams : bigram markhov chain
+    """
+
+    s = s.split(' ') # make the string into a list
+    agenda = [ [['['],[],['mg'],False] ]  # initialise the agenda
+    parses = [] # this will be our output
+    while len(agenda)>0: # keep parsing as long as we have incomplete parses
+        for parse in agenda:
+            print("\nparse: %s"%parse)
+            i=len(parse[0])
+            if i==len(s): # move a complete parse over to the output
+                parses.append(parse)
+                agenda.remove(parse)
+            else:
+                if len(parse[1])>0: # if buffer not empty
+                    ## try clearing the buffer
+                    print ("\n Clear")
+                    if not parse[3]:  #i if last special op not clear
+                        new_parse=[[],[],[],[]] # this is to make copies of the lists inside the list
+                        new_parse[0]=parse[0][:] # copy the string
+                        new_parse[2]=parse[2][:] # copy the list of operations
+                        new_parse[1]=[] # clear the buffer
+                        new_parse[2].append('clear')
+                        new_parse[3]=True # the last special op was Clear
+                        print ("new parse: %s"%new_parse)
+                        agenda.append(new_parse) # add this new parse to the agenda
+
+                    #Try to Copy
+                    print ("\n Copy")
+                    print ("buffer ",parse[1])
+                    print ("copy? ", s[i : i+len(parse[1])])
+                    # if the buffer is the same as the next part of the sentence
+                    if i+len(parse[1]) <= len(s) and parse[1]==s[i : i+len(parse[1])]: 
+                        new_parse=[[],[],[],[]] # make a deep copy
+                        new_parse[2]=parse[2][:] # copy the list of operations
+                        new_parse[0]=s[:i+len(parse[1])] # skip ahead to end of copy in the string
+                        new_parse[1]=parse[1][:]+parse[1][:] #add copy to buffer
+                        new_parse[2].append('copy') # add operation to operation list 
+                        new_parse[3]=False # the last special operation was not Clear
+                        print ("new parse : %s"%new_parse)
+                        agenda.append(new_parse) # add the new parse to the agenda
+
+                # Try to Merge
+                print ("\n Merge")
+                if s[i] in bigrams[s[i-1]]:
+                    parse[0].append(s[i]) #string
+                    parse[1].append(s[i]) #buffer
+                    parse[2].append('mg') #list of ops
+                    print ("merged parse: %s"%parse)
+                else:
+                    return (False,parses) # if this isn't a legal transition, this sentence is not grammatical
+
+    return (True,parses)
+
+
+
+def probability(s,bigrams,ops):
+    """
+    Parses a string and give the total probability of the parses.
+
+    Returns two different things: a total probability for all parses
+    and a total probability only for parses that don't have a Clear that isn't followed by a Copy.
+    
+    Uses an agenda and a list of complete parses.
+    We initialise the agenda with a start state,   [['['],[],['mg'],False,0.]
+    An agenda item is a list of 5 elements:
+    the string so far, the buffer, the operations, and whether the last "special" operation (copy or clear) is clear.
+    The second-last element is a little hack to reduce the amount of pointless buffer-clearing.
+    the last is the probability of the parse
+    We add onto an agenda item until it is a complete parse, in which case we move it to the output, parses
+    Whenever there is a choice of two valid moves, we copy the agenda item and try both
+        
+    Arguments:
+    s       : the string to be parsed -- string
+    bigrams : bigram markhov chain
+    ops     : operations markhov chain (mg,copy,clear)
+    """
+
+    s = s.split(' ') # make the string into a list
+    agenda = [ [['['],[],['mg'],False,0.] ]  # initialise the agenda
+    parses = ((log0,[]),(log0,[])) # these will be our outputs: (sum prob,parses). First is all parses, second excludes parses with pointless buffer-clearing
+    while len(agenda)>0: # keep parsing as long as we have incomplete parses. We have a chance to bail if there's a bad transition
+        for parse in agenda:
+            print("\nparse: %s"%parse)
+            i=len(parse[0])
+            if i==len(s): # move a complete parse over to the output
+                (all_parses,useful_parses)=parses 
+                all_parses = (log_add(all_parses[0],parse[4]), all_parses[1]+[parse]) # add prob of this parse to total prob
+                copy_clear = [op for op in parse[2] if op !='mg'] # get just the non-merge operations
+                useful=True
+                for i in range(len(copy_clear)): # look for an instance of clear not followed by a copy
+                    #print ("i=%i"%i)
+                    #print (copy_clear)
+                    if copy_clear[i]=='clear' and (i==len(copy_clear)-1 or copy_clear[i+1] !='copy') :
+                        useful=False # if you find one this isn't a useful parse
+                        break
+                if useful:
+                    useful_parses = (log_add(useful_parses[0],parse[4]), useful_parses[1]+[parse])
+                parses=(all_parses,useful_parses)
+                agenda.remove(parse)
+            else:
+                if len(parse[1])>0: # if buffer not empty
+                    ## try clearing the buffer
+                    print ("\n Clear")
+                    if not parse[3]:  #i if last special op not clear
+                        new_parse=[[],[],[],[],[]] # this is to make copies of the lists inside the list
+                        new_parse[0]=parse[0][:] # copy the string
+                        new_parse[2]=parse[2][:] # copy the list of operations
+                        new_parse[1]=[] # clear the buffer
+                        new_parse[2].append('clear')
+                        new_parse[3]=True # the last special op was Clear
+                        new_parse[4]=parse[4]+ops[parse[2][-1]]['clear']
+                        print ("new parse: %s"%new_parse)
+                        agenda.append(new_parse) # add this new parse to the agenda
+
+                    #Try to Copy
+                    print ("\n Copy")
+                    print ("buffer ",parse[1])
+                    print ("copy? ", s[i : i+len(parse[1])])
+                    # if the buffer is the same as the next part of the sentence
+                    if i+len(parse[1]) <= len(s) and parse[1]==s[i : i+len(parse[1])]: 
+                        new_parse=[[],[],[],[],[]] # make a deep copy
+                        new_parse[2]=parse[2][:] # copy the list of operations
+                        new_parse[0]=s[:i+len(parse[1])] # skip ahead to end of copy in the string
+                        new_parse[1]=parse[1][:]+parse[1][:] #add copy to buffer
+                        new_parse[2].append('copy') # add operation to operation list 
+                        new_parse[3]=False # the last special operation was not Clear
+                        last_op=parse[2][-1]
+                        print last_op
+                        new_parse[4]=parse[4]+ops[last_op]['copy']
+                        print ("new parse : %s"%new_parse)
+                        agenda.append(new_parse) # add the new parse to the agenda
+
+                # Try to Merge
+                print ("\n Merge")
+                if s[i] in bigrams[s[i-1]]:
+                    parse[0].append(s[i]) #string
+                    parse[1].append(s[i]) #buffer
+                    parse[2].append('mg') #list of ops
+                    parse[4]=parse[4]+ops[parse[2][-1]]['mg']+bigrams[s[i-1]][s[i]]
+
+                    print ("merged parse: %s"%parse)
+                else:
+                    return parses # if this isn't a legal transition, this sentence is not grammatical
+
+    return parses
+
+
